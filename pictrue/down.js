8618b9ff15c9200877c8a4d7970394062acf14b9
superce <562923778@qq.com>
2020-05-26 23:17:50 +0800

    caiji

--- pictrue/down.js
+++ pictrue/down.js
@@ -3,30 +3,28 @@ const request = require('request')
 const path = require('path')
 const {guid} = require('../guid/index')
 
-
-//创建文件夹目录
-var dirPath = path.join(__dirname, "../filepictrue");
-console.log(__dirname)
-if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
-    console.log("文件夹创建成功");
-} else {
-    console.log("文件夹已存在");
-}
-
 /*
 * url 网络文件地址
 * filename 文件名
 * callback 回调函数
 */
-
-function downFile(url) {
-    // let guid = '123'+'.jpg'
-    // console.log(guid)
+function downFile(url,i) {
+    //创建文件夹目录
+    let namepath = `../filepictrue${i}`
+    var dirPath = path.join(__dirname, namepath);
+    console.log(__dirname)
+    if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath);
+        console.log("文件夹创建成功");
+    } else {
+        console.log("文件夹已存在");
+    }
+    let filename = guid()+'.jpg'
+    console.log(filename)
     return new Promise(function (resolve, reject) {
         request(url, function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                let stream = fs.createWriteStream(path.join(dirPath, '123'+'.jpg'));
+                let stream = fs.createWriteStream(path.join(dirPath, filename));
                 request(url).pipe(stream).on("close", function (err) {
                     resolve(`下载成功,图片名称`);
                     console.log(dirPath)
@@ -43,7 +41,7 @@ function downFile(url) {
 
 }
 
-exports.down = async function(url){
+exports.down = async function(url,i){
     console.log(url)
-    downFile(url)
+   await downFile(url,i)
 }
--- pictrue/index.js
+++ pictrue/index.js
@@ -17,17 +17,56 @@ async function addPictrueUrl(href){
     return add
 }
 
+function pictrue_list() {
+    let url = 'https://www.tupianzj.com/meinv/yishu/list_178_1.html'
+    return new Promise(resolve => {
+        let urlarray = []
+        superagent.get(url).charset('utf-8').buffer(true).end((err, res) => {
+            if (err) {
+                console.log(err)
+                console.log(33)
+            } else {
+                let html = res.text
+                let $ = $html.$html(html)
+                $('.list_con_box ul.list_con_box_ul li').each(async (ind, item) => {
+                    let href = $(item).children('a').attr('href')
+                    let _href = `https://www.tupianzj.com${href}`
+                    urlarray.push(_href)
+                })
+                resolve(urlarray)
+            }
+        })
+    })
+}
+function pageNum(url) { 
+    return new Promise(resolve => {
+        superagent.get(url).charset('utf-8').buffer(true).end((err, res) => {
+            if (err) {
+                console.log(err)
+                console.log(33)
+            } else {
+                let html = res.text
+                let $ = $html.$html(html)
+                let page = $('.pages ul li').eq(0).children('a').text()
+                let num = page.match(/\d+/g)[0]
+                // let img = $('.pic_tupian a>img').attr('src')
+                resolve(num)
+            }
+        })
+    })
+}
 function getPictrueUrl(url){
     return new Promise(resolve =>{
-        superagent.get(url).charset('utf-8').buffer(true).end(async (err,res)=>{
-            console.log(987)
+        superagent.get(url).charset('utf-8').buffer(true).end((err,res)=>{
             if(err){
                 console.log(err)
                 console.log(33)
             }else{
                 let html = res.text
                 let $ = $html.$html(html)  
-                let img = $('.floatItem .wrap img').attr('src')
+                let page = $('.pages ul li').eq(0).children('a').text()
+                num = page.match(/\d+/g)[0]
+                let img = $('.pic_tupian a>img').attr('src')
                 resolve(img)
             }
         })
@@ -35,7 +74,31 @@ function getPictrueUrl(url){
 }
 
 exports.pictureUrl = async function(){
-    let url = config.url
-    let res = await getPictrueUrl(url)
-    down.down(res)
+    let arrayurl = await pictrue_list()
+    console.log(arrayurl)
+    for (let i=0;i<arrayurl.length;i++){
+        console.log('循环')
+        console.log(i)
+        let index = await pageNum(arrayurl[i])
+        console.log('----------------------------------------------')
+        console.log(index)
+        for (let i = 0; i < index+1; i++) {
+            console.log(i)
+            let _url = arrayurl[i]
+            if (i > 0) {
+                let j = i
+                if(i===1){
+                    j++
+                }
+                console.log('i大于零')
+                let _split = _url.split('')
+                let num = `_${j}`
+                _split.splice(-5, 0, num)
+                _url = _split.join('')
+            }
+            console.log(_url)
+            let res = await getPictrueUrl(_url)
+            down.down(res, i)
+        }
+    }
 }
